dfd404403afb562beeacb74701a08c2aa282e5fc
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-01-17 01:00:20 -0800

    feat(project): prepare testing infrastructure (#19)
    - fixes #18
    - adds project-wide scripts

---
+++ modules/services/database/index.ts
@@ -0,0 +1,7 @@
+import { CreateOrganizationTable1736855400685 } from "./migrations/1736855400685-create_organization_table";
+import { CreateSchoolTable1736961079489 } from "./migrations/1736961079489-create_school_table";
+
+export const Migrations = [
+    CreateOrganizationTable1736855400685,
+    CreateSchoolTable1736961079489
+];
