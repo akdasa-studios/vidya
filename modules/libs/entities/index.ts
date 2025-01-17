bd0a69cdf8c587f9537ff2208c96e101cdd4a2cd
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-01-17 10:17:37 +0000

    feat(database): add tests

--- modules/libs/entities/index.ts
+++ modules/libs/entities/index.ts
@@ -1,7 +1,23 @@
 import { Organization } from './organization';
 import { School } from './school';
-import { Course } from './course';
-import { Group } from './group';
+import { Course, LearningType } from './course';
+import { Group, GroupStatus } from './group';
 import { Lesson } from './lesson';
-export const Entities = [Organization, School, Course, Group, Lesson];
-export { Organization, School, Course, Group, Lesson };
+
+export {
+  Course,
+  Group,
+  GroupStatus,
+  LearningType,
+  Lesson,
+  Organization,
+  School
+};
+
+export const Entities = [
+  Course,
+  Group,
+  Lesson,
+  Organization,
+  School
+];
---
+++ modules/tests/unit/database/course.spec.ts
@@ -0,0 +1,90 @@
+import { Organization, School, Course, LearningType } from "@vidya/entities";
+import { QueryFailedError } from "typeorm";
+import { dataSource } from "./helpers/dataSource";
+
+describe('Course', () => {
+  let org: Organization;
+  let school: School;
+
+  beforeEach(async () => {
+    org = new Organization();
+    org.name = 'organization';
+    await dataSource.manager.save(org);
+
+    school = new School();
+    school.name = 'school';
+    school.organization = org;
+    await dataSource.manager.save(school);
+  });
+
+  test('should create a course', async () => {
+    // Arrange
+    const course = new Course();
+    course.name = 'test course';
+    course.school = school;
+
+    // Act
+    const result = await dataSource.manager.save(course);
+
+    // Assert
+    expect(result.id).toBeDefined();
+    expect(result.schoolId).toBe(school.id);
+    expect(result.school.name).toBe(school.name);
+  });
+
+  test('should default learningType to individual', async () => {
+    // Arrange
+    const course = new Course();
+    course.name = 'test course';
+    course.school = school;
+
+    // Act
+    const result = await dataSource.manager.save(course);
+
+    // Assert
+    expect(result.learningType).toBe(LearningType.Individual);
+  });
+
+  it.each([
+    LearningType.Individual,
+    LearningType.Group
+  ])('should set learningType to "%s"', async (value: LearningType) => {
+    // Arrange
+    const course = new Course();
+    course.name = 'test course';
+    course.school = school;
+    course.learningType = value;
+
+    // Act
+    const result = await dataSource.manager.save(course);
+
+    // Assert
+    expect(result.learningType).toBe(value);
+  });
+
+  test('should raise error for invalid learningType', async () => {
+    // Arrange
+    const course = new Course();
+    course.name = 'test course';
+    course.school = school;
+    course.learningType = 'invalidType' as LearningType;
+
+    // Act
+    const save = async () => await dataSource.manager.save(course);
+
+    // Assert
+    await expect(save).rejects.toThrow(QueryFailedError);
+  });
+
+  test('should raise error if no school specified', async () => {
+    // Arrange
+    const course = new Course();
+    course.name = 'test course';
+
+    // Act
+    const save = async () => await dataSource.manager.save(course);
+
+    // Assert
+    await expect(save).rejects.toThrow(QueryFailedError);
+  });
+});
