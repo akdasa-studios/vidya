8165dbd83ec39fa32a019ccbb02dd293c441e899
Advaita Krishna das <advaita.krishna.das@gmail.com>
2025-02-28 09:55:15 +0000

    refactor(api): introduce ScopedOrganizationsRequest for user permission handling

--- modules/services/api/src/org/services/organizations.service.ts
+++ modules/services/api/src/org/services/organizations.service.ts
@@ -14,18 +14,36 @@ export class OrganizationsService extends EntitiesService<Organization> {
     super(repository);
   }
 
-  async findPermittedOrganizationsBy(
-    permissions: UserPermissions,
-    filter?: {
-      id?: string;
-    },
-  ): Promise<Organization[]> {
-    const orgIds = permissions.getPermittedOrganizations(['orgs:read']);
+  /**
+   * Retuns a scoped request for the current user permissions.
+   * @param userPermissions User permissions.
+   * @returns Scoped request.
+   */
+  scopedBy(userPermissions: UserPermissions): ScopedOrganizationsRequest {
+    return new ScopedOrganizationsRequest(userPermissions, this);
+  }
+}
+
+class ScopedOrganizationsRequest {
+  constructor(
+    private readonly userPermissions: UserPermissions,
+    private readonly service: OrganizationsService,
+  ) {}
+
+  /**
+   * Returns all organizations that the user has permission to read.
+   * @param filter Filter to apply.
+   * @returns Organizations.
+   */
+  async findAllBy(filter?: { id?: string }): Promise<Organization[]> {
+    const orgIds = this.userPermissions.getPermittedOrganizations([
+      'orgs:read',
+    ]);
     const filterOrgIds = filter?.id
       ? [filter.id].filter((id) => orgIds.includes(id))
       : orgIds;
 
-    return await this.repository.find({
+    return await this.service.findAllBy({
       where: {
         id: In(filterOrgIds),
       },
