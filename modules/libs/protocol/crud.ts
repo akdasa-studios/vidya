75d7e1a1fe324e2fb07a85ab511a260e0a4290c0
Advaita Krishna das <advaita.krishna.das@gmail.com>
2025-03-02 17:22:16 +0000

    refactor(protocol): improve CRUD type definitions and add documentation

--- modules/libs/protocol/crud.ts
+++ modules/libs/protocol/crud.ts
@@ -1,17 +1,15 @@
 /* -------------------------------------------------------------------------- */
-/*                                   Models                                   */
-/* -------------------------------------------------------------------------- */
-
-export type ItemWithId<TIdType> = {
-  id: TIdType;
-}
-
-/* -------------------------------------------------------------------------- */
 /*                                   Creaate                                  */
 /* -------------------------------------------------------------------------- */
 
+/**
+ * Request to create an item.
+ */
 export type CreateItemRequest<TItemType> = TItemType;
 
+/**
+ * Response to creating an item.
+ */
 export type CreateItemResponse<TIdentityType> = {
   id: TIdentityType;
 }
@@ -38,14 +36,23 @@ export type GetItemResponse<TItemType> = TItemType;
 /*                                   Update                                   */
 /* -------------------------------------------------------------------------- */
 
+/**
+ * Request to update an item.
+ */
 export type UpdateItemRequest<TItemType> = Partial<TItemType>;
 
+/**
+ * Response to updating an item.
+ */
 export type UpdateItemResponse<TItemType> = Partial<TItemType>;
 
 /* -------------------------------------------------------------------------- */
 /*                                   Delete                                   */
 /* -------------------------------------------------------------------------- */
 
+/**
+ * Request to delete an item.
+ */
 export type DeleteItemResponse = {
   success: boolean;
 }
